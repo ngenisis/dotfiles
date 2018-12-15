95f87cce20bafcc47ba9a228df8dd8bd610a99c8
ngenisis <ngenisis@gmail.com>
2018-12-15 11:42:35 -0600

    Moved bash prompt configs to separate file that gets loaded in .bashrc

---
+++ .bashrc.d/prompt.bash
@@ -0,0 +1,50 @@
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+# Define colors
+GREEN="\[\033[01;32m\]"
+BLUE="\[\033[01;34m\]"
+MAGENTA="\[\033[01;35m\]"
+NONE="\[\033[00m\]"
+
+# Configure git prompt
+GIT_PS1_SHOWDIRTYSTATE=yes
+GIT_PS1_SHOWSTASHSTATE=yes
+GIT_PS1_SHOWUNTRACKEDFILES=yes
+GIT_PS1_SHOWUPSTREAM=verbose
+GIT_PS1_HIDE_IF_PWD_IGNORED=yes
+source ~/.git-prompt.sh
+
+if [ "$color_prompt" = yes ]; then
+    PROMPT_COMMAND='__git_ps1 "$GREEN\u@\h$NONE:$BLUE\w$MAGENTA" "$NONE\n\$ "'
+else
+    PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\n\$"'
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
