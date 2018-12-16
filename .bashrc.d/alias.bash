32de3e7e70ab3662f2440b3595a0992589799fb6
ngenisis <ngenisis@gmail.com>
2018-12-16 00:26:15 -0600

    Removed sudo ranger alias

--- .bashrc.d/alias.bash
+++ .bashrc.d/alias.bash
@@ -13,11 +13,10 @@ alias ll='ls -l'
 alias la='ls -A'
 alias l='ls -CF'
 alias o='xdg-open'
-alias r='ranger-cd'
-alias sr='sudo ranger'
 alias sa='sudo apt'
 alias sag='sudo apt-get'
 alias sdnf='sudo dnf'
+alias r='ranger-cd'
 
 # Vim alias. Tries to use neovim, vim, and then vi, in that order
 if [ -x /usr/bin/nvim ]; then
