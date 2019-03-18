411b8430b51f544d1358f50892924575effe2a71
ngenisis <ngenisis@gmail.com>
2019-03-17 23:59:14 -0500

    Don't test for color

--- sh/.config/sh/alias.sh
+++ sh/.config/sh/alias.sh
@@ -1,14 +1,10 @@
 # Enable color
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls -hN --color=auto --group-directories-first'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
+alias ls='ls -hN --color=auto --group-directories-first'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 alias ll='ls -l'
 alias la='ls -A'
