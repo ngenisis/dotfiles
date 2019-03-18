0004847605628ae6d87c93313c3d02b17082e63b
ngenisis <ngenisis@gmail.com>
2019-03-17 23:40:04 -0500

    Add colors for less

--- sh/.config/sh/environment.sh
+++ sh/.config/sh/environment.sh
@@ -32,3 +32,13 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 if [ -x /usr/bin/rg ] ; then
 	export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --smart-case --glob "!.git/*"'
 fi
+
+# Colors for less (i.e. man pages)
+# See man terminfo
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 1)
+export LESS_TERMCAP_md=$(tput bold; tput setaf 1)
+export LESS_TERMCAP_me=$(tput sgr0; tput rmso; tput rmul)
+export LESS_TERMCAP_se=$(tput sgr0; tput rmso; tput rmul)
+export LESS_TERMCAP_so=$(tput bold; tput setab 4; tput setaf 3)
+export LESS_TERMCAP_ue=$(tput sgr0; tput rmso; tput rmul)
+export LESS_TERMCAP_us=$(tput bold; tput setaf 2)
